a3a2a4597903038a26093b60621267448dbe2fe8
root <root@kali.skyn3t.nl>
2021-02-19 22:59:57 +0100

    print fix

--- Modules/logparser.py
+++ Modules/logparser.py
@@ -2,7 +2,7 @@
 import sys
 
 if len(sys.argv) != 2:
-    print "Usage: logparser.py <textfile>"
+    print("Usage: logparser.py <textfile>")
     sys.exit(0)
 
 ip = sys.argv[1]
@@ -11,5 +11,5 @@ protocol = sys.argv[2]
 def logparsertxt(results):
     for line in results:
         if ("|" in line) or (" . " in line):
-                print '\033[1;32m[+]  ' + line + '\033[1;m'
+                print('\033[1;32m[+]  ' + line + '\033[1;m')
     return
